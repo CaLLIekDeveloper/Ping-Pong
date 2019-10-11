d2ce48a8eae96b5d51eda7c875ae082cc8e46edb
Александр Паршин <parshin_sashek@mail.ru>
2019-10-11 23:29:39 +0300

    Добавлены две вьюшки обучения

--- app/src/main/java/com/CaLLIek/pingpong/TutorialFirstActivity.java
+++ app/src/main/java/com/CaLLIek/pingpong/TutorialFirstActivity.java
@@ -2,9 +2,13 @@ package com.CaLLIek.pingpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class TutorialFirstActivity extends AppCompatActivity {
 
@@ -16,5 +20,16 @@ public class TutorialFirstActivity extends AppCompatActivity {
         //убираем строку состояния андроида
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageView btnNext = findViewById(R.id.btn_next_tutorial1);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TutorialFirstActivity.this,TutorialSecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
