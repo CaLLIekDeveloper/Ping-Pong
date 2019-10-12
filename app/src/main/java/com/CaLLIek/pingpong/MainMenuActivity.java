d84c742911c565625b8be75ae34e28a565f33bdb
Александр Паршин <parshin_sashek@mail.ru>
2019-10-12 15:31:04 +0300

    Фиксы + Добавленно главное меню

--- app/src/main/java/com/CaLLIek/pingpong/MainMenuActivity.java
+++ app/src/main/java/com/CaLLIek/pingpong/MainMenuActivity.java
@@ -1,4 +1,4 @@
-package com.CaLLIek.pingpong.Tutorial;
+package com.CaLLIek.pingpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,27 +9,27 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.CaLLIek.pingpong.MainMenuActivity;
-import com.CaLLIek.pingpong.R;
+import com.CaLLIek.pingpong.Tutorial.TutorialFirstActivity;
 
-public class TutorialFifthActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutorial_fifth);
+        setContentView(R.layout.activity_main_menu);
+
 
 
         //убираем строку состояния андроида
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        ImageView btnNext = findViewById(R.id.btn_next_tutorial_ok);
+        ImageView btnNext = findViewById(R.id.main_menu_btn_info);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), MainMenuActivity.class);
+                Intent intent = new Intent(getBaseContext(), TutorialFirstActivity.class);
                 startActivity(intent);
                 finish();
             }
