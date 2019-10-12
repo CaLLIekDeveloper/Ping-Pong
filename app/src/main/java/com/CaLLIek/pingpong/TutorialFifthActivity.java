287697aa2e69db6c0c67b39cc40afcbb1410621c
Александр Паршин <parshin_sashek@mail.ru>
2019-10-12 11:18:07 +0300

    Добавлена пятая сцена обучения

--- app/src/main/java/com/CaLLIek/pingpong/TutorialFifthActivity.java
+++ app/src/main/java/com/CaLLIek/pingpong/TutorialFifthActivity.java
@@ -9,24 +9,24 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
-public class TutorialFourthActivity extends AppCompatActivity {
+public class TutorialFifthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutorial_fourth);
+        setContentView(R.layout.activity_tutorial_fifth);
 
 
         //убираем строку состояния андроида
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        ImageView btnNext = findViewById(R.id.btn_next_tutorial4);
+        ImageView btnNext = findViewById(R.id.btn_next_tutorial_ok);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(),TutorialFifthActivity.class);
+                Intent intent = new Intent(getBaseContext(),TutorialFirstActivity.class);
                 startActivity(intent);
                 finish();
             }
