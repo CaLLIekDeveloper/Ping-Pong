512bbd3439117398e70174491e748784def8b12a
Александр Паршин <parshin_sashek@mail.ru>
2019-10-18 18:36:24 +0300

    Добавлена сцена тренировки

---
+++ app/src/main/java/com/CaLLIek/pingpong/TraineActivity.java
@@ -0,0 +1,14 @@
+package com.CaLLIek.pingpong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TraineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traine);
+    }
+}
